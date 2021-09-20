50bff7b7065853ad10ba29eb1e45f03d0b7e4367
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-20 15:31:55 -0500

    Solved squared

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -27,3 +27,8 @@ function calcAge(age) {
 function convert(minutes) {
   return minutes * 60;
 }
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+function squared(b) {
+  return b * b;
+}
