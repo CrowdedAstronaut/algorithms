d852db4b4c50c74f200eff3a308e9e893f259735
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-20 15:43:07 -0500

    Solved Less than 100

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -57,3 +57,12 @@ function printArray(number) {
 
   return newArray;
 }
+
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+function lessThan100(a, b) {
+  if (a + b < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
