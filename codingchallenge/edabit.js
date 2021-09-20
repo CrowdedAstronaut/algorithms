a50c3027fff8b54b9d631baf3e041709690b7900
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-20 15:23:11 -0500

    Solved giveMeSomething

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -3,3 +3,8 @@
 function findPerimeter(length, width) {
   return length * 2 + width * 2;
 }
+
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+  return `something ${a}`;
+}
