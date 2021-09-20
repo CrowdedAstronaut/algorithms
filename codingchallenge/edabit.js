183264777d6eba108b381816d1d80eb256e63d49
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-20 15:39:52 -0500

    Solved buggy code number 5

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -46,3 +46,14 @@ function triArea(base, height) {
 function addition(a, b) {
   return a + b;
 }
+
+//Mubashir created an infinite loop! Help him by fixing the code in the code tab to pass this challenge. Look at the examples below to get an idea of what the function should do.
+function printArray(number) {
+  let newArray = [];
+
+  for (let i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+}
