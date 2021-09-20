4990d1663cb4a9f3e9d2928db3f7b17b647a1d9a
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-20 15:46:10 -0500

    Solved remainder

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -66,3 +66,8 @@ function lessThan100(a, b) {
     return false;
   }
 }
+
+//There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+function remainder(x, y) {
+  return x % y;
+}
