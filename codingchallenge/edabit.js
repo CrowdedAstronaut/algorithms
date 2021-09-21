d2ca480936ce1c59e48e14eed0aa8cc70fffdcb6
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-21 11:50:09 -0500

    Solved remainder

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -76,3 +76,8 @@ function remainder(x, y) {
 function cubes(a) {
   return a ** 3;
 }
+
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+function remainder(x, y) {
+  return x % y;
+}
