450ce4389c862a629e88f7bf92d6514e06dac834
CrowdedAstronaut <ross@johnrossphillips.com>
2021-09-21 11:48:51 -0500

    Solved buggy code challenge

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -71,3 +71,8 @@ function lessThan100(a, b) {
 function remainder(x, y) {
   return x % y;
 }
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+function cubes(a) {
+  return a ** 3;
+}
