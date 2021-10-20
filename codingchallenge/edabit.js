8b6a1596ef52cd187193088e014c6aafeeeb4c0b
CrowdedAstronaut <ross@johnrossphillips.com>
2021-10-20 16:10:47 -0500

    Solved volume of box

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -351,7 +351,6 @@
 
 // // const sum = (n) => (n === 1 ? n : n + sum(n - 1));
 
-<<<<<<< HEAD
 // // yeah_nope = (bool) => (true ? "yeah" : "nope");
 
 // // // modify the template variable to be a template string
@@ -508,150 +507,149 @@
 // const circle_or_square = (rad, area) =>
 //   2 * pi * rad > Math.sqrt(area) * 4 ? true : false;
 
-<<<<<<< HEAD
-const addBinary = (a, b) => (a + b).toString(2);
-=======
-equilibrium = (x) => (x > 0 ? "positive" : x < 0 ? "negative" : true);
+// equilibrium = (x) => (x > 0 ? "positive" : x < 0 ? "negative" : true);
 
-const incrementItems = (arr) => arr.map((n) => n + 1);
+// const incrementItems = (arr) => arr.map((n) => n + 1);
 
-function futurePeople(population, n) {}
+// function futurePeople(population, n) {}
 
-calculateExponent = (num, exp) => num ** exp;
+// calculateExponent = (num, exp) => num ** exp;
 
-shouldServeDrinks = (age, onBreak) =>
-  age >= 18 && onBreak == !true ? true : false;
+// shouldServeDrinks = (age, onBreak) =>
+//   age >= 18 && onBreak == !true ? true : false;
 
-imposterFormula = (i, p) => Math.round(100 * (i / p)) + "%";
+// imposterFormula = (i, p) => Math.round(100 * (i / p)) + "%";
 
-const arr = [1, 2, 3, 4, 5, 6];
-let a = arr[0];
-let b = arr[1];
+// const arr = [1, 2, 3, 4, 5, 6];
+// let a = arr[0];
+// let b = arr[1];
 
-leapYear = (year) => (year % 4 === 0 || year % 400 === 0 ? true : false);
+// leapYear = (year) => (year % 4 === 0 || year % 400 === 0 ? true : false);
 
-operate = (num1, num2, operator) => eval(num1, operator, num2);
+// operate = (num1, num2, operator) => eval(num1, operator, num2);
 
-function getLastItem(arr) {
-  return arr.length[-1];
-}
+// function getLastItem(arr) {
+//   return arr.length[-1];
+// }
 
-minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
+// minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
 
-const sum = (n) => (n === 1 ? n : n + sum(n - 1));
+// const sum = (n) => (n === 1 ? n : n + sum(n - 1));
 
-yeah_nope = (bool) => (true ? "yeah" : "nope");
+// yeah_nope = (bool) => (true ? "yeah" : "nope");
 
-// modify the template variable to be a template string
-function format(a, b, c) {
-  // the result string must give: "Their names were: a, b and c."
-  const template = `Their names were: ${a}, ${b} and ${c}.`;
-  return template;
-}
+// // modify the template variable to be a template string
+// function format(a, b, c) {
+//   // the result string must give: "Their names were: a, b and c."
+//   const template = `Their names were: ${a}, ${b} and ${c}.`;
+//   return template;
+// }
 
-//Given an index and an array, return the value of the array with the given index.
+// //Given an index and an array, return the value of the array with the given index.
 
-isOdd = (num) => (num % 2 === 0 ? false : true);
+// isOdd = (num) => (num % 2 === 0 ? false : true);
 
-//Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
-inchesToFeet = (inches) => (inches < 12 ? 0 : inches / 12);
+// //Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
+// inchesToFeet = (inches) => (inches < 12 ? 0 : inches / 12);
 
-//Is the String Odd or Even?
-oddOrEven = (s) => (s.length % 2 === 0 ? true : false);
+// //Is the String Odd or Even?
+// oddOrEven = (s) => (s.length % 2 === 0 ? true : false);
 
-//Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers
-isEvenOrOdd = (num) => (num % 2 === 0 ? "even" : "odd");
+// //Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers
+// isEvenOrOdd = (num) => (num % 2 === 0 ? "even" : "odd");
 
-//Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
-function areaShape(base, height, shape) {
-  if (shape === "triangle") {
-    return 0.5 * base * height;
-  } else return base * height;
-}
+// //Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
+// function areaShape(base, height, shape) {
+//   if (shape === "triangle") {
+//     return 0.5 * base * height;
+//   } else return base * height;
+// }
 
-function concat(arr1, arr2) {
-  return arr1.concat(arr2);
-}
+// function concat(arr1, arr2) {
+//   return arr1.concat(arr2);
+// }
 
-function flipBool(b) {
-  if (b == true) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
+// function flipBool(b) {
+//   if (b == true) {
+//     return 0;
+//   } else {
+//     return 1;
+//   }
+// }
 
-//Create a function that takes an array of numbers or letters and returns a string
-function arrayToString(arr) {
-  return arr.join("").toString();
-}
+// //Create a function that takes an array of numbers or letters and returns a string
+// function arrayToString(arr) {
+//   return arr.join("").toString();
+// }
 
-function fiftyThirtyTwenty(ati) {
-  return {
-    Needs: 0.5 * ati,
-    Wants: 0.3 * ati,
-    Savings: 0.2 * ati,
-  };
-}
+// function fiftyThirtyTwenty(ati) {
+//   return {
+//     Needs: 0.5 * ati,
+//     Wants: 0.3 * ati,
+//     Savings: 0.2 * ati,
+//   };
+// }
 
-function search(arr, item) {
-  return arr.indexOf(item);
-}
+// function search(arr, item) {
+//   return arr.indexOf(item);
+// }
 
-function newWord(str) {
-  return str.slice(1);
-}
+// function newWord(str) {
+//   return str.slice(1);
+// }
 
-function sumArray(arr) {
-  const reducer = (previousValue, currentValue) => previousValue + currentValue;
-  return arr.reduce(reducer);
-}
+// function sumArray(arr) {
+//   const reducer = (previousValue, currentValue) => previousValue + currentValue;
+//   return arr.reduce(reducer);
+// }
 
-function findIndex(arr, str) {
-  return arr.indexOf(str);
-}
+// function findIndex(arr, str) {
+//   return arr.indexOf(str);
+// }
 
-function operate(num1, num2, operator) {
-  return eval(num1 + operator + num2);
-}
+// function operate(num1, num2, operator) {
+//   return eval(num1 + operator + num2);
+// }
 
-const colorInvert = (rgb) => rgb.map((x) => 255 - x);
+// const colorInvert = (rgb) => rgb.map((x) => 255 - x);
 
-//Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
-const helloName = (name) => `Hello ${name}!`;
+// //Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
+// const helloName = (name) => `Hello ${name}!`;
 
-//Write a function to check if an array contains a particular number.
-function check(arr, el) {
-  return arr.includes(el);
-}
+// //Write a function to check if an array contains a particular number.
+// function check(arr, el) {
+//   return arr.includes(el);
+// }
 
-//Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
-const arrBetween = (a, b, c) => c.filter((x) => x > a && x < b);
+// //Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
+// const arrBetween = (a, b, c) => c.filter((x) => x > a && x < b);
 
-//Create a function that returns a number, based on the string provided. Here is a list of all digits (if you are non-English speaker):
-function word(s) {
-  let obj = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    zero: 0,
-  };
+// //Create a function that returns a number, based on the string provided. Here is a list of all digits (if you are non-English speaker):
+// function word(s) {
+//   let obj = {
+//     one: 1,
+//     two: 2,
+//     three: 3,
+//     four: 4,
+//     five: 5,
+//     six: 6,
+//     seven: 7,
+//     eight: 8,
+//     nine: 9,
+//     zero: 0,
+//   };
 
-  return obj[s];
-}
+//   return obj[s];
+// }
 
-//Create a function that takes in an array of numbers and returns the sum of its cubes.
-const sumOfCubes = (nums) => {
-  return nums.reduce((sum, num) => {
-    return sum + Math.pow(num, 3);
-  }, 0);
-};
+// //Create a function that takes in an array of numbers and returns the sum of its cubes.
+// const sumOfCubes = (nums) => {
+//   return nums.reduce((sum, num) => {
+//     return sum + Math.pow(num, 3);
+//   }, 0);
+// };
 
+// const parity = (n) => (n%2 == '0') ? 'even': 'odd';
 
-const parity = (n) => (n%2 == '0') ? 'even': 'odd';
+const volumeOfBox = ({ width, length, height }) =>
+  width * length * height;
