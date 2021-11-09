041a887f676ba13161c8475d1719c2a4ee4596e9
CrowdedAstronaut <ross@johnrossphillips.com>
2021-11-09 14:48:39 -0600

    Solved getContainer

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -685,5 +685,39 @@
 
 // absolute = (n) => (n < 1 ? n * -1 : n);
 
-isLastCharacterN = (word) =>
-  word.slice(-1) === "n" ? true : false;
+// isLastCharacterN = (word) =>
+//   word.slice(-1) === "n" ? true : false;
+/* 
+The packaging system is running wild! The candy is lying loose all over in the warehouse, the cereal is missing, and bread is stuffed in a bottle. What is going on here? The candy should be in plastic and the bread should be in a bag.
+
+The packaging machine is running the getContainer() function to retrieve the container of a product. But something is not right... */
+
+function getContainer(product) {
+  let container;
+  switch (product) {
+    case "Bread":
+      container = "bag";
+      break;
+    case "Beer":
+      container = "bottle";
+      break;
+    case "Milk":
+      container = "bottle";
+      break;
+    case "Cerials":
+      container = "box";
+      break;
+    case "Eggs":
+      container = "carton";
+      break;
+    case "Candy":
+      container = "plastic";
+      break;
+    default:
+      container = null;
+  }
+
+  return container;
+}
+
+console.log(getContainer("Bread"));
