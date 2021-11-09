08b71e9c603673af3393ed0c5dd2e93afbdd1a85
CrowdedAstronaut <ross@johnrossphillips.com>
2021-11-09 12:47:11 -0600

    Solved absolute

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -673,12 +673,14 @@
 //     : str.replace("amazing", "not amazing");
 // }
 
-function frontThree(str) {
-  if (str.length < 3) {
-    return str.repeat(3);
-  } else {
-    return str.slice(0, 3).repeat(3);
-  }
-}
+// function frontThree(str) {
+//   if (str.length < 3) {
+//     return str.repeat(3);
+//   } else {
+//     return str.slice(0, 3).repeat(3);
+//   }
+// }
 
-console.log(frontThree("Python"));
+// console.log(frontThree("Python"));
+
+absolute = (n) => (n < 1 ? n * -1 : n);
