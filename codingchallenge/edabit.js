d3b57475744ade9714ad63b9e3b858ec606fa9d5
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-01 08:10:23 -0600

    Solved diagonal Difference

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -887,10 +887,29 @@ The packaging machine is running the getContainer() function to retrieve the con
 //   return ar.reduce((a, b) => a + b, 0);
 // }
 
-function aVeryBigSum(ar) {
-  let sum = 0;
-  for (let i = 0; i < ar.length; i++) {
-    sum += ar[i];
+// function aVeryBigSum(ar) {
+//   let sum = 0;
+//   for (let i = 0; i < ar.length; i++) {
+//     sum += ar[i];
+//   }
+//   return sum;
+// }
+
+function diagonalDifference(arr) {
+  let n = arr.length;
+  let diag1 = 0;
+  let diag2 = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      // an element from the main diagonal
+      if (i === j) {
+        diag1 += arr[i][j];
+      }
+      // an element from the counterdiagonal
+      if (i + j === n - 1) {
+        diag2 += arr[i][j];
+      }
+    }
   }
-  return sum;
+  return Math.abs(diag1 - diag2);
 }
