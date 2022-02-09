980c76b06fe15946d118ad72288ae8afe6fdc2f5
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-09 14:26:22 -0600

    Solved nSidedShape

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -1048,3 +1048,38 @@ The packaging machine is running the getContainer() function to retrieve the con
 // function bitwiseXOR(n1, n2) {
 //   return n1 ^ n2;
 // }
+
+function nSidedShape(n) {
+  switch (n) {
+    case 1:
+      return "circle";
+      break;
+    case 2:
+      return "semi-circle";
+      break;
+    case 3:
+      return "triangle";
+      break;
+    case 4:
+      return "square";
+      break;
+    case 5:
+      return "pentagon";
+      break;
+    case 6:
+      return "hexagon";
+      break;
+    case 7:
+      return "heptagon";
+      break;
+    case 8:
+      return "octagon";
+      break;
+    case 9:
+      return "nonagon";
+      break;
+    case 10:
+      return "decagon";
+      break;
+  }
+}
