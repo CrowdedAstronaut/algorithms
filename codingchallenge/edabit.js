cac53f03938100523aea4108c2cf765e3bdb7425
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-21 14:05:57 -0600

    solved largest swap

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -1276,3 +1276,24 @@ The packaging machine is running the getContainer() function to retrieve the con
 //   }
 //   return result;
 // }
+
+// function plusMinus(arr) {
+//   let pos = 0,
+//     neg = 0,
+//     zero = 0,
+//     length = arr.length;
+//   arr.forEach((n) => {
+//     if (n > 0) pos++;
+//     else if (n < 0) neg++;
+//     else zero++;
+//   });
+//   console.log((pos / length).toFixed(6));
+//   console.log((neg / length).toFixed(6));
+//   console.log((zero / length).toFixed(6));
+// }
+
+function largestSwap(num) {
+  return (
+    num.toString().charAt(0) >= num.toString().charAt(1)
+  );
+}
