6de3458b99cabb4d6a78f6f7e490b34aa3a050dd
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-09 11:20:18 -0600

    Solved sign your name

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -1406,3 +1406,14 @@ The packaging machine is running the getContainer() function to retrieve the con
 
 //   return newArr;
 // }
+
+function signYourName(obj) {
+  // write your code here
+  // don't use a return statement
+  Object.seal(obj);
+
+  // DON'T CHANGE OR REMOVE THE LINES BELOW
+  obj.yourSignature = "Whatever";
+  obj.spouseSignature = "Nice Try";
+  return obj;
+}
