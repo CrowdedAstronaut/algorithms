258539b0f03cbba9162ea83c81b9b5e5e9c38a2a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-15 12:00:03 -0500

    Solved isAvgWhole

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -1618,3 +1618,11 @@ console.log(q.getPerimeter());
 //   }
 //   return "There is no bomb, relax.";
 // }
+
+function isAvgWhole(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return Number.isInteger(sum / arr.length);
+}
