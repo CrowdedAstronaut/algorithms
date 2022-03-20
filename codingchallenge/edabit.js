3fd43d537cbb3c4895b5f48f12d40d9f3bb0ac52
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-20 17:33:33 -0500

    Solved Calculator oop

--- codingchallenge/edabit.js
+++ codingchallenge/edabit.js
@@ -1877,8 +1877,24 @@ console.log(q.getPerimeter());
 //   return (n * (n + 1)) / 2;
 // }
 
-const weightAllowed = (car, passengers, maxWeight) =>
-  car + passengers.reduce((x, y) => x + y) < 2.2 * maxWeight;
+// const weightAllowed = (car, passengers, maxWeight) =>
+//   car + passengers.reduce((x, y) => x + y) < 2.2 * maxWeight;
 
-const skipTooMuchSugarDrinks = (drinks) =>
-  drinks.filter((drink) => drink !== "cola" && drink !== "fanta");
+// const skipTooMuchSugarDrinks = (drinks) =>
+//   drinks.filter((drink) => drink !== "cola" && drink !== "fanta");
+
+class Calculator {
+  // Write functions to add(), subtract(), multiply() and divide()
+  add(num1, num2) {
+    return num1 + num2;
+  }
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+  divide(num1, num2) {
+    return num1 / num2;
+  }
+}
