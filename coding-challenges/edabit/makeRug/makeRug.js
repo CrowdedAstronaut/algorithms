a95ea5139fae54ebdf11cec17aab905e52145091
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-22 15:46:13 -0500

    Solved makerug

--- coding-challenges/edabit/makeRug/makeRug.js
+++ coding-challenges/edabit/makeRug/makeRug.js
@@ -1,5 +1,5 @@
-function makeRug(m, n, s = "#") {
+const makeRug = (m, n, s = "#") => {
   let arr = [];
-  for (let i = 0; i < m; i++) a.push(s.repeat(n));
+  for (let i = 0; i < m; i++) arr.push(s.repeat(n));
   return arr;
-}
+};
