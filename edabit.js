6e0404a63b2e963c4e79bb0b9f7e28fd26b274d8
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-22 14:33:37 -0500

    Solved fill

--- edabit.js
+++ edabit.js
@@ -2081,10 +2081,37 @@ console.log(q.getPerimeter());
 //   return arr.join(":");
 // }
 
-const squaresSum = (n) => {
-  let sum = 0;
-  for (let i = 0; i <= n; i++) {
-    sum += i * i;
+// const squaresSum = (n) => {
+//   let sum = 0;
+//   for (let i = 0; i <= n; i++) {
+//     sum += i * i;
+//   }
+//   return sum;
+// };
+/* 
+According to the lodash documentation, _.fill Fills elements of an array with the value from start to, but not including, end. Note that this method mutates the array.
+
+This challenge requires you to write your own version of this function without using lodash so that you can better understand it works.
+
+Arguments
+array (Array): The array to fill.
+value (*): The value to fill array with.
+[start=0] (number): The start position.
+[end=array.length] (number): The end position.
+Returns
+(Array): Returns array.
+Examples
+var array = [1, 2, 3]
+
+fill(array, "a") ➞ ["a", "a", "a"]
+
+fill(Array(3), 2) ➞ [2, 2, 2]
+
+fill([4, 6, 8, 10], "*", 1, 3) ➞ [4, "*", "*", 10]
+ */
+const fill = (arr, value, start = 0, end = arr.length) => {
+  for (let i = start; i < end; i++) {
+    arr[i] = value;
   }
-  return sum;
+  return arr;
 };
