a1a37083d3ee6b852d6a6c5612d597bec4f9bffa
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-26 17:45:50 -0500

    Solved removeNumbers

---
+++ coding-challenges/edabit/decimalPart/README.md
@@ -0,0 +1,16 @@
+### A Simple Task
+
+Mubashir needs your help in a simple task.
+
+Create a function that takes a number n and return its decimal part.
+
+Examples
+decimalPart(1.2) ➞ 0.2
+
+decimalPart(-3.73) ➞ 0.73
+
+decimalPart(10) ➞ 0
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
--- coding-challenges/hackerrank/2D_Array_DS/hourglassSum.js
+++ coding-challenges/hackerrank/2D_Array_DS/hourglassSum.js
@@ -1,3 +1,26 @@
 function hourglassSum(arr) {
+  let array = [],
+    count = -63;
   // Write your code here
+  for (let array_i = 0; array_i < 6; array_i++) {
+    array[array_i] = readLine().split(" ");
+    array[array_i] = array[array_i].map(Number);
+  }
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let sum;
+      sum =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      if (sum > count) {
+        count = sum;
+      }
+    }
+  }
+  return count;
 }
