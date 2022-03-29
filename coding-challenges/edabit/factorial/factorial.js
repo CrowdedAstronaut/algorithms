3e7f6085066147ba168cd6005ec1fd7951bf11ab
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-28 20:22:50 -0500

    Solved factorialize

--- coding-challenges/edabit/factorial/factorial.js
+++ coding-challenges/edabit/factorial/factorial.js
@@ -1,1 +1,7 @@
-const factorial = (int) => (int <= 1 ? 1 : factorial(num - 1));
+function factorial(int) {
+  if (int < 0) return -1;
+  else if (int == 0) return 1;
+  else {
+    return int * factorial(int - 1);
+  }
+}
