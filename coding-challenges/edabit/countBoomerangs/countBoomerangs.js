fbc187add91b47774448e12da26d12eb4d2e1621
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-03 16:13:38 -0500

    Solved number of boomerangs

---
+++ coding-challenges/edabit/countBoomerangs/countBoomerangs.js
@@ -0,0 +1,40 @@
+//My solution
+
+const countBoomerangs = (arr) => {
+  let a = [];
+  let b = [];
+  for (let i = 0; i < arr.length; i++) {
+    //console.log("i:" + i);
+    if (arr[i] === arr[i + 2] && arr[i + 1] !== arr[i]) {
+      //console.log("arr[i]: " + arr[i] + " & arr[i + 2]:" + arr[i + 2]);
+      a.push(arr[i]);
+      a.push(arr[i + 1]);
+      a.push(arr[i + 2]);
+      //console.log("a: " + a);
+      b.push(a);
+      //console.log("b:" + b);
+      a = [];
+    }
+  }
+  let c = b.length;
+  return c;
+};
+
+//Edabit Solution
+function countBoomerangs(arr) {
+  return arr.reduce(
+    (acc, e, index) => acc + (e === arr[index + 2] && e != arr[index + 1]),
+    0
+  );
+}
+
+//Edabit Solution
+const countBoomerangs = (arr) => {
+  let collection = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 2] && arr[i] != arr[i + 1]) {
+      collection++;
+    }
+  }
+  return collection;
+};
