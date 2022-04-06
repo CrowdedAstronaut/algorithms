d89a2c57315590ad58b897bfab7ef58eb6622bdc
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-06 16:34:48 -0500

    solve hackerrank

--- coding-challenges/edabit/oddEven/oddEven.js
+++ coding-challenges/edabit/oddEven/oddEven.js
@@ -11,3 +11,17 @@ const oddeven = (arr) => {
   }
   return evenArray.length < oddArray.length ? true : false;
 };
+
+//Edabit Solutions forEach()
+function oddeven(arr) {
+  let odd = 0;
+  let even = 0;
+
+  arr.forEach((num) => (num % 2 == 0 ? even++ : odd++));
+  return odd > even;
+}
+
+//Edabit solution for .filter()
+function oddeven(arr) {
+  return arr.filter((ele) => ele % 2 !== 0).length > arr.length / 2;
+}
