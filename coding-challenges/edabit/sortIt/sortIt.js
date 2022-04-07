e4a2802e5ec45ddd974a3c720ade20f316685a47
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-07 18:28:02 -0500

    Solved sort it

---
+++ coding-challenges/edabit/sortIt/sortIt.js
@@ -0,0 +1,7 @@
+//Edabit solution 1
+
+const sortIt = (arr) => arr.sort((a, b) => (a[0] || a) - (b[0] || b));
+
+//Edabit solution 2
+const sortIt2 = (arr) =>
+  arr.sort((a, b) => a - (typeof b === "object" ? b[0] : b));
