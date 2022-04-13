5c72181ee5d909876496c221ca66b240e5c92dc0
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-13 15:30:30 -0500

    Notebook rebase

--- coding-challenges/hackerrank/kangaroo/kangaroo.js
+++ coding-challenges/hackerrank/kangaroo/kangaroo.js
@@ -7,3 +7,10 @@ function kangaroo(x1, v1, x2, v2) {
     return "NO";
   }
 }
+
+const kangaroo = (x1, v1, x2, v2) => {
+  let jump = (x2 - x1) / (v2 - v1);
+  if ((x1 > x2 && v1 >= v2) || (x2 > x1 && v2 >= v1)) return "NO";
+
+  return jump % 1 === 0 ? "YES" : "NO";
+};
