371b2735f2269768d12d78bd810e57a45b54b6b7
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-18 13:29:44 -0500

    solved amplify

---
+++ coding-challenges/edabit/amplify/amplify.js
@@ -0,0 +1,21 @@
+function amplify(num) {
+  let array = [];
+  for (let i = 1; i <= num; i++) {
+    let j = i;
+    if (i % 4 === 0) {
+      j *= 10;
+    }
+    array.push(j);
+  }
+  return array;
+}
+
+console.log(amplify(25));
+
+const amplify = (num) => {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    result.push(i);
+  }
+  return result.map((x) => (x % 4 == 0 ? x * 10 : x));
+};
