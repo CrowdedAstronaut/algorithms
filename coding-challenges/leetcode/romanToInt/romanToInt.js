69d74e67d23f28036b27da44487ba9f103b57da5
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-27 11:42:40 -0500

    Solved romanToInt

---
+++ coding-challenges/leetcode/romanToInt/romanToInt.js
@@ -0,0 +1,29 @@
+const romanToInt = (s) => {
+  const obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  const arr = Array.from(s);
+  const empty = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      (arr[i] == "I" &&
+        (arr[i + 1] == "V" || arr[i + 1] == "X")) ||
+      (arr[i] == "X" &&
+        (arr[i + 1] == "L" || arr[i + 1] == "C")) ||
+      (arr[i] == "C" &&
+        (arr[i + 1] == "D" || arr[i + 1] == "M"))
+    ) {
+      empty.push(-obj[arr[i]]);
+    } else {
+      empty.push(obj[arr[i]]);
+    }
+  }
+  const result = empty.reduce((curr, acc) => curr + acc);
+  return result;
+};
