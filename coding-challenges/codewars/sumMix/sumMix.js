2061463be3a27e3b49c082d5b8e4b3e4573cf768
CrowdedAstronaut <ross@johnrossphillips.com>
2022-07-06 12:11:08 -0500

    desktop commit

---
+++ coding-challenges/codewars/sumMix/sumMix.js
@@ -0,0 +1,2 @@
+const sumMix = (x) =>
+  x.map((item) => Number(item)).reduce((a, b) => a + b);
---
+++ coding-challenges/codewars/updateLight/updateLight.js
@@ -0,0 +1,8 @@
+const updateLight = (current) =>
+  current === "green"
+    ? "yellow"
+    : current === "yellow"
+    ? "red"
+    : "green";
+
+//your code here!
