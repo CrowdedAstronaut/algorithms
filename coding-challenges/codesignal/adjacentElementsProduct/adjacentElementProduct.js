b269d018f35689f30da1e4fc7de5d50d37115de4
CrowdedAstronaut <ross@johnrossphillips.com>
2022-07-20 12:27:49 -0500

    Added CodeSignal

---
+++ coding-challenges/codesignal/adjacentElementsProduct/adjacentElementProduct.js
@@ -0,0 +1,18 @@
+const { performance } = require("perf_hooks");
+
+let startTime = new Date().getTime();
+function adjacentElementsProduct(arr) {
+  return Math.max(...arr.slice(0, -1).map((item, idx) => item * arr[idx + 1]));
+}
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+let endTime = new Date().getTime();
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
+// console.time(function adjacentElementsProduct(array) {
+//   let maxProduct = array[0] * array[1];
+//   for (let i = 1; i < array.length; i++) {
+//     product = array[i] * array[i + 1];
+//     if (product > maxProduct) maxProduct = product;
+//   }
+//   return maxProduct;
+// });
--- coding-challenges/codewars/charCodeCalculation/charCodeCalculation.js
+++ coding-challenges/codewars/charCodeCalculation/charCodeCalculation.js
@@ -1,17 +1,4 @@
 function calc(x) {
-<<<<<<< HEAD
-  let ASCII = [...x]
-    .map((char) => char.charCodeAt())
-    .join("");
-  let ASCIISum = [...ASCII]
-    .map((character) => Number(character))
-    .reduce((a, b) => a + b);
-  let total2 = ASCII.replace(/7/g, "1");
-  let total2Sum = [...total2]
-    .map((character) => Number(character))
-    .reduce((a, b) => a + b);
-  return ASCIISum - total2Sum;
-=======
   let total1 = [...x].map((char) => char.charCodeAt(...x));
   let total1String = total1.join("");
   let total1Sum = total1String
@@ -26,7 +13,6 @@ function calc(x) {
   // let total2Sum = total2.reduce((a, b) => a + b);
   // total2Sum = total2Array.reduce((a, b) => parseInt(a) + parseInt(b));
   return total2Sum;
->>>>>>> dffd169 (Laptop commit)
 }
 
 console.log(calc("ABC"));
