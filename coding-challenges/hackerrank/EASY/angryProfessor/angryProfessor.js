08618ee9eb7729a2e953d0a263a495901c2f2a93
CrowdedAstronaut <ross@johnrossphillips.com>
2022-07-21 20:02:56 -0500

    passing most test cases for angry professor

--- coding-challenges/hackerrank/EASY/angryProfessor/angryProfessor.js
+++ coding-challenges/hackerrank/EASY/angryProfessor/angryProfessor.js
@@ -1,11 +1,10 @@
 function angryProfessor(k, a) {
-  let late = [a].filter((item) => Number(item) > 0);
-  //   if (late.length < k) {
-  //     return "YES";
-  //   } else {
-  //     return "NO";
-  //   }
-  return late;
+  let late = [...a].filter((item) => Number(item) >= 0);
+  if (late.length < k) {
+    return "YES";
+  } else {
+    return "NO";
+  }
 }
 
-console.log(angryProfessor(4, [-2, -1, 0, 1, 2]));
+console.log(angryProfessor(4, [-93, -86, 49, -62, -90, -63, 40, 72, 11, 67]));
