8c480fbec91c851caee179f561ab17005f435d2b
ross phillips <ross@johnrossphillips.com>
2023-08-21 13:31:02 -0500

    added addition

--- coding-challenges/hackerrank/10DaysOfJS/Day0/plusMinus/plusMinus.js
+++ coding-challenges/hackerrank/10DaysOfJS/Day0/plusMinus/plusMinus.js
@@ -1,8 +1,11 @@
+let arr = [-4, 3, -9, 0, 4, 1];
 function plusMinus(arr) {
   let plus = [...arr].filter((item) => item > 0);
   let minus = [...arr].filter((item) => item < 0);
   let zero = [...arr].filter((item) => item === 0);
   console.log(plus / arr.length).toFixed(6);
-  console.log(minus / arr.length).toFixed(6);
-  console.log(zero / arr.length).toFixed(6);
+  // console.log(minus / arr.length).toFixed(6);
+  // console.log(zero / arr.length).toFixed(6);
 }
+
+console.log(plusMinus(arr));
